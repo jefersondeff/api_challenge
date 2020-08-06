b8a66acd6c0c54965aed3b7faa6edd19ee661a28
jefersondeff <jefersonactf@gmail.com>
2020-08-06 12:32:39 -0300

    Adicionado constantes para calculo de multa

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,14 @@
-# constant from books
+# this constants represent book status
 BOOK_AVAILABLE = "Disponível"
 BOOK_BORROWED = "Emprestado"
+
+# this constants represent percent
+ZERO_PERCENT = 0
+THREE_PERCENT = 3
+FIVE_PERCENT = 5
+SEVEN_PERCENT = 7
+
+# this constants represent days of delay
+ZERO_DAYS = 0
+THREE_DAYS = 3
+FIVE_DAYS = 5
